83b2d45555d08afdf9fcca994a7854d24b725b1b
Breno Kazuo <92324471+brenotoyo@users.noreply.github.com>
2026-05-14 18:21:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,61 @@
-function Jogo(nome, genero, anoDeLançamento) {
+function Jogo(nome, genero, anoDeLançamento, qtdJogadores) {
     this.nome = nome;
     this.genero = genero;
     this.anoDeLançamento = anoDeLançamento;
+
+    let _qtdJogadores = qtdJogadores;
+
+    this.getJogadores = function() {
+        return _qtdJogadores;
+    }
+
+    this.setJogadores = function (valor) {
+        if (typeof valor === 'number') {
+            _qtdJogadores = valor;
+        }
+    }
+
 }
 
-function Estrategia(nome, personagens) {
-    Jogo.call(this, nome, 'Moba', 2009);
+function Estrategia(nome, qtdJogadores) {
+    Jogo.call(this, nome, 'Moba', 2009, qtdJogadores);
+
+    this.aumentoJogadores = function(){
+        const novosJogadores = this.getJogadores() * 3;
+
+        this.setJogadores(novosJogadores);
+    }
 }
 
-function Tiro(nome){
-    Jogo.call(this, nome, "Fps", 2020);
+function Tiro(nome, qtdJogadores){
+    Jogo.call(this, nome, "Fps", 2020, qtdJogadores);
+
+    this.aumentoJogadores = function(){
+        const novosJogadores = this.getJogadores() * 10;
+
+        this.setJogadores(novosJogadores);
+    }
 }
 
-const jogo1 = new Jogo("Minecraft", "Sobrevivência", 2009);
-const jogo2 = new Estrategia("League of Legends", 140);
-const jogo3 = new Tiro("Valorant");
+const jogo1 = new Jogo("Minecraft", "Sobrevivência", 2009, 1);
+const jogo2 = new Estrategia("League of Legends", 100);
+const jogo3 = new Tiro("Valorant", 5);
+const jogo4 = new Tiro("Counter Strike", 9);
 
 console.log(Object.values(jogo1));
 console.log(Object.values(jogo2));
 console.log(Object.values(jogo3));
+console.log(Object.values(jogo4));
+
+console.log(jogo1.getJogadores());
+console.log(jogo2.getJogadores());
+console.log(jogo3.getJogadores());
+console.log(jogo4.getJogadores());
+
+jogo2.aumentoJogadores();
+jogo3.aumentoJogadores();
+
+console.log(jogo1.getJogadores());
+console.log(jogo2.getJogadores());
+console.log(jogo3.getJogadores());
+console.log(jogo4.getJogadores());
