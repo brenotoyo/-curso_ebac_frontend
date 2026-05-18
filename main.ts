e1322a71c003dc83f57421bf66ce2b4fedd23e98
Breno Kazuo <92324471+brenotoyo@users.noreply.github.com>
2026-05-18 13:11:39 -0300

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,10 @@
+function multplicacao (valor1: number, valor2: number): number {
+    return valor1 * valor2;
+}
+
+function saudar(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+console.log(multplicacao(2, 3));
+console.log(saudar("Breno"));
